bf47d04d902c92f455ed63d30208be3537d37531
Owen Ekhator <oe52@gmail.com>
2018-05-29 17:10:49 -0500

    completed

--- stage-3/src/index.js
+++ stage-3/src/index.js
@@ -8,8 +8,8 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 
 ReactDOM.render(
-
-    <Provider>
+    
+    <Provider store={store}>
         <HashRouter>
             <App />
         </HashRouter>
--- stage-3/src/redux/store.js
+++ stage-3/src/redux/store.js
@@ -1,4 +1,6 @@
+import {createStore, applyMiddleware} from 'redux'
+import promiseMiddleware from 'redux-promise-middleware';
 import reducer from './reducer';
-import reduxPromiseMiddleware from 'redux-promise-middleware';
 
-export default createStore(reducer, applyMiddleware(reduxPromiseMiddleware()));
+
+export default createStore(reducer, applyMiddleware(promiseMiddleware()));
